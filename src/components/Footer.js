3d582431394eb574cdc0b5c965c0790306e4791a
C0dingWithT0ny <78626437+C0dingWithT0ny@users.noreply.github.com>
2023-03-16 18:49:28 -0700

    Header And Footer created

---
+++ src/components/Footer.js
@@ -0,0 +1,75 @@
+import React from "react";
+import {
+Box,
+Container,
+Row,
+Column,
+FooterLink,
+Heading,
+} from "./FooterStyles";
+
+const Footer = () => {
+return (
+	<Box>
+	<h1 style={{ color: "green",
+				textAlign: "center",
+				marginTop: "-50px" }}>
+		Company - Slogan
+	</h1>
+	<Container>
+		<Row>
+		<Column>
+			<Heading>About Us</Heading>
+			<FooterLink href="http://localhost:3001/about-us/aim">Aim</FooterLink>
+			<FooterLink href="http://localhost:3001/about-us/vision">Vision</FooterLink>
+			<FooterLink href="http://localhost:3000/about-us/Testimonials">Testimonials</FooterLink>
+		</Column>
+		<Column>
+			<Heading>Services</Heading>
+			<FooterLink href="http://localhost:3001/services/services1">Writing</FooterLink>
+			<FooterLink href="http://localhost:3001/services/services2">Internships</FooterLink>
+			<FooterLink href="http://localhost:3001/services/services3">Coding</FooterLink>
+		</Column>
+		<Column>
+			<Heading>Contact Us</Heading>
+			<FooterLink href="http://localhost:3001/contact">Uttar Pradesh</FooterLink>
+			<FooterLink href="http://localhost:3001/contact">Ahemdabad</FooterLink>
+			<FooterLink href="http://localhost:3001/contact">Indore</FooterLink>
+		</Column>
+		<Column>
+			<Heading>Social Media</Heading>
+			<FooterLink href="#">
+			<i className="fab fa-facebook-f">
+				<span style={{ marginLeft: "10px" }}>
+				Facebook
+				</span>
+			</i>
+			</FooterLink>
+			<FooterLink href="#">
+			<i className="fab fa-instagram">
+				<span style={{ marginLeft: "10px" }}>
+				Instagram
+				</span>
+			</i>
+			</FooterLink>
+			<FooterLink href="#">
+			<i className="fab fa-twitter">
+				<span style={{ marginLeft: "10px" }}>
+				Twitter
+				</span>
+			</i>
+			</FooterLink>
+			<FooterLink href="https://www.youtube.com/@Cryptoforyou">
+			<i className="fab fa-youtube">
+				<span style={{ marginLeft: "10px" }}>
+				Youtube
+				</span>
+			</i>
+			</FooterLink>
+		</Column>
+		</Row>
+	</Container>
+	</Box>
+);
+};
+export default Footer;
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export const AboutUs = () => {
+return (
+	<div className="home">
+	<h1> About us</h1>
+	</div>
+);
+};
+
+export const OurAim = () => {
+return (
+	<div className="home">
+	<h1> Aim</h1>
+	</div>
+);
+};
+
+export const OurVision = () => {
+	return (
+		<div className="home">
+		<h1>Vision</h1>
+		</div>
+	);
+};
+export const Testimonials = () => {
+	return (
+		<div className="home">
+		<h1>Testimonials</h1>
+		</div>
+	);
+};
